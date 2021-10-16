90b789a292a0e1134997d14368703a91a5a047f0
Sugam Kumar Singh <72367458+SugamKumarSingh@users.noreply.github.com>
2021-10-17 01:59:28 +0530

    Create subscribe.js

---
+++ subscribe.js
@@ -0,0 +1,35 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuKc81YtOx-dJnWyQ0qjCZggQjmJCfV6Q",
+    authDomain: "iysd-web.firebaseapp.com",
+    projectId: "iysd-web",
+    storageBucket: "iysd-web.appspot.com",
+    messagingSenderId: "113725570166",
+    appId: "1:113725570166:web:d9a866af869eb9052f7357",
+    measurementId: "G-VT5FF48BVL"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
+const db = firebase.firestore();
+
+function subMe() {
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+
+    var docRef = db.collection("subscribers").doc(email);
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            console.log("email", email, "is already registered...");
+        } else {
+            docRef.set({
+                email: email,
+                iKEY: "########"
+            });
+            console.log("Congratulations, you are now registered for our website.");
+        }
+    }).catch((error) => {
+        console.log("We faced an error please retry:", error);
+    });
+
+}
